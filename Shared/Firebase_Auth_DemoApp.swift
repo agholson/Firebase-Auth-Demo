b0d637056fa7aee70b82413235c3605928aff037
Andrew Gholson <test@me.com>
2021-12-28 19:40:36 -0500

    Initial Commit

---
+++ Shared/Firebase_Auth_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_Auth_DemoApp.swift
+//  Shared
+//
+//  Created by Leone on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_Auth_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
